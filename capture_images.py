798c38a98cd36b8c5dd8116d984d595fb5842739
henriarm <henriquegabrielyan@gmail.com>
2025-02-13 20:52:11 +0100

    Added script to capture images

---
+++ capture_images.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+
+
+def create_images_directory(directory="images"):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def capture_images(video_source=1, directory="images"):
+    create_images_directory(directory)
+    cap = cv2.VideoCapture(video_source)
+    num = 0
+
+    while cap.isOpened():
+        success, img = cap.read()
+        if not success:
+            break
+
+        k = cv2.waitKey(5)
+        if k == 27:
+            break
+        elif k == ord("s"):  # wait for 's' key to save and exit
+            cv2.imwrite(f"{directory}/img{num}.png", img)
+            print("image saved!")
+            num += 1
+
+        cv2.imshow("Img", img)
+
+    # Release and destroy all windows before termination
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    capture_images()
